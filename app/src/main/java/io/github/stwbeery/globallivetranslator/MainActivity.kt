e54268ba89255ae9a1f04c5ff6504548b69dbb13
stwbeery <stwbeery@users.noreply.github.com>
2026-07-18 21:34:15 +0800

    fix: use scoped Compose weight modifier

--- app/src/main/java/io/github/stwbeery/globallivetranslator/MainActivity.kt
+++ app/src/main/java/io/github/stwbeery/globallivetranslator/MainActivity.kt
@@ -29,7 +29,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
